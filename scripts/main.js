5e47f27e707208603ed1211b18ee6aea85bbb2f1
Sam Odell <7421902@isd535.org>
2022-04-22 11:32:18 -0500

    Messing around

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
-// Put your JavaScript here
+function setup() {
+    createCanvas(1200, 400);
+} 
+
+function draw() {
+    beginShape();
+        vertex(random(12,100), random(12,100));
+        endShape(CLOSE);
+
+}
+
+function mousePressed(){
+    noLoop();
+}
+
+function keyPressed(space){
+    loop();
+} 
+
