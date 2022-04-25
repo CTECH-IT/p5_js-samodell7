39a907668bf23b1784e7f083a908465481fdf00b
Sam Odell <7421902@isd535.org>
2022-04-25 11:17:34 -0500

    progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
+//kinda makes a space ship
 function setup() {
-    createCanvas(1200, 400);
-} 
-
-function draw() {
-    beginShape();
-        vertex(random(12,100), random(12,100));
-        endShape(CLOSE);
-
-}
-
-function mousePressed(){
-    noLoop();
-}
-
-function keyPressed(space){
-    loop();
-} 
-
+    createCanvas(300, 300);
+  }
+  
+  function draw() {
+    background(32);
+    ship(100,200,50,50);
+  }
+  
+  function ship(bodyX,bodyY,bodyW,bodyH){
+  fill(255,255,255)
+  rect(bodyX,bodyY,bodyW,bodyH)
+    
+    fill(255,0,0)
+    triangle(bodyX,bodyY,bodyX+bodyW/2,height-bodyY+40,bodyX+bodyW,bodyY)
+    
+    fill(255,100,0); triangle(bodyX+5,height-bodyX/2,bodyX+bodyW/2,height-bodyX+bodyX*.9,bodyX+bodyW-5,height-bodyX/2)
+  }
