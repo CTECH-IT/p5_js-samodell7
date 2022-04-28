b9a46a523da6b24cf4289854f1fe4850cff9d451
samodell7 <98342504+samodell7@users.noreply.github.com>
2022-04-28 08:01:33 -0500

    cool swirl art

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 let img;
 
 function preload(){
-img = loadImage('img/Dlo.webp');
+img = loadImage('img/minn.jpeg');
 }
 
 function setup() {
@@ -10,17 +10,19 @@ function setup() {
   let newCanvasX = (windowWidth- img.width)/2;
   let newCanvasY = (windowHeight- img.height)/2;
   cnv.position(newCanvasX, newCanvasY);
-  for(let col = 0; col< img.width; col++){
-    for(let row = 0; row < img.height; row++){
+  for(let col = 0; col< img.width; col+=.2){
+    for(let row = 0; row < img.height; row+=.2){
       let xPos = col;
       let yPos = row;
       let c = img.get(xPos,yPos);
       push();
       translate(xPos, yPos);
-      stroke(color(c));
-      strokeWeight(60);
-      point(col,row);
-      rect(col, row, 25, 25);
+      rotate(random(radians(360)));
+      noFill();
+      strokeWeight(random(2));
+      stroke(color(c))
+      curve(xPos, yPos, sin(xPos)*random(20), cos(xPos) * sin(xPos) * random(20), random(10), random(20), cos(yPos) *sin(xPos) * random(20), cos(yPos) * sin(xPos) *random(20)) 
+      pop();
     }
   }
 }
