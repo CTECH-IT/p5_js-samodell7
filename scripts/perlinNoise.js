9b30a453f27253ea66eb44e7ff6ea68f57a616ad
Sam Odell <7421902@isd535.org>
2022-04-29 11:11:22 -0500

    asd

--- scripts/perlinNoise.js
+++ scripts/perlinNoise.js
@@ -1,6 +1,9 @@
-var inc = 0.01;
-var scl=20;
+var inc = 0.1;
+var scl=10;
 var cols, rows;
+var zoff = 0;
+
+var fr;
 
 function setup() {
   let wX = windowWidth-40;
@@ -8,23 +11,33 @@ function setup() {
   createCanvas(wX, wY);
   cols = floor(width/scl);
   rows = floor(height/scl);
+  fr = createP('');
 }
 
 function draw() {
+  background(255);
+  randomSeed(10);
   let yoff = 0;
   for (let y = 0; y < rows; y++) {
     let xoff = 0;
     for (let x = 0; x < cols; x++) {
       let index = (x + y * width) * 4;
       // let r = random(255);
-      let r = noise(xoff, yoff) * 255;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      var v = p5.Vector.fromAngle(angle);
       xoff += inc;
        
-      fill (map(noise(255)));
-      rect(x*scl, y*scl, scl, scl);
+      stroke(0);
+      push();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      line(0, 0, scl, 0);
+      
+      pop();
     }
     yoff += inc;
+    zoff += .0001
   }
 
-
+ fr.html(floor(frameRate()));
 }
